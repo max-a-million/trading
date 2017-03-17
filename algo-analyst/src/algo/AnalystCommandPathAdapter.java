ffaccdaa4be40d2eb8957e9c78a8bacd2ff0ba7b
max-a-million <smartt43@gmail.com>
2017-03-17 20:51:28 +0300

    gitignore

--- algo-analyst/src/algo/AnalystCommandPathAdapter.java
+++ algo-analyst/src/algo/AnalystCommandPathAdapter.java
@@ -5,7 +5,6 @@ import datp.network.v.remoting.client.RemoteClientCommandPathApiAdapter;
 public class AnalystCommandPathAdapter extends RemoteClientCommandPathApiAdapter {
 	
 	public AnalystCommandPathAdapter(String uri) {
-		
 		super();
 		initialize(uri);
 	}
